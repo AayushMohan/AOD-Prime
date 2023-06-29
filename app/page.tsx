a92457b9a3ac74e1a79e10884782a847c682dff9
AayushMohan <aayushmohan1702@gmail.com>
2023-06-29 19:14:51 +0530

    Added Metrics Component

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import FAQ from "@/components/FAQ";
 import Hero from "@/components/Hero";
+import Metrics from "@/components/Metrics";
 
 export default function Home() {
   return (
     <div>
       <Hero />
       <FAQ />
+      <Metrics />
     </div>
   );
 }
