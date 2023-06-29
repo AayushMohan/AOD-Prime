0177fc8e1facad366f7b00f1117417744b0d7014
AayushMohan <aayushmohan1702@gmail.com>
2023-06-29 23:27:40 +0530

    Added 1st metrices details

--- components/Metrics.tsx
+++ components/Metrics.tsx
@@ -4,7 +4,10 @@ const Metrics = () => {
   return (
     <div className="flex-row h-screen mx-28">
       <div className="flex items-center">
-        <div className="p-40 mx-10 bg-[#0D0D0D] rounded-3xl"></div>
+        <div className="p-40 mx-10 bg-[#0D0D0D] rounded-3xl">
+          <h2>1.2</h2>
+          <span>Million Collisions</span>
+        </div>
         <div className="p-40 mx-10 bg-[#0D0D0D] rounded-3xl"></div>
         <div className="p-40 mx-10 bg-[#0D0D0D] rounded-3xl"></div>
       </div>
