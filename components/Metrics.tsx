1eaf8512aa6c9efb5b34ecefd05da6ad0cdca287
AayushMohan <aayushmohan1702@gmail.com>
2023-06-29 23:28:18 +0530

    Updated Styling

--- components/Metrics.tsx
+++ components/Metrics.tsx
@@ -4,7 +4,7 @@ const Metrics = () => {
   return (
     <div className="flex-row h-screen mx-28">
       <div className="flex items-center">
-        <div className="p-40 mx-10 bg-[#0D0D0D] rounded-3xl">
+        <div className="p-40 mx-10 bg-[#0D0D0D] rounded-3xl text-center">
           <h2>1.2</h2>
           <span>Million Collisions</span>
         </div>
