88727791471fc08dc5a6061515f602ef7e84f466
AayushMohan <aayushmohan1702@gmail.com>
2023-06-29 19:31:50 +0530

    Added Matrics Container

--- components/Metrics.tsx
+++ components/Metrics.tsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 const Metrics = () => {
-  return <div>Metrics</div>;
+  return (
+    <div className="flex-row h-screen mx-28">
+      <div className="flex items-center">
+        <div className="p-40 mx-10 bg-[#0D0D0D] rounded-3xl"></div>
+        <div className="p-40 mx-10 bg-[#0D0D0D] rounded-3xl"></div>
+        <div className="p-40 mx-10 bg-[#0D0D0D] rounded-3xl"></div>
+      </div>
+    </div>
+  );
 };
 
 export default Metrics;
