324283c38e84deb54e21ed2db6ee9b76d948e6df
AayushMohan <aayushmohan1702@gmail.com>
2023-06-29 23:30:16 +0530

    Updated Fonts

--- components/Metrics.tsx
+++ components/Metrics.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Metrics = () => {
   return (
     <div className="flex-row h-screen mx-28">
-      <div className="flex items-center">
+      <div className="flex items-center font-sans">
         <div className="p-40 mx-10 bg-[#0D0D0D] rounded-3xl text-center">
           <h2>1.2</h2>
           <span>Million Collisions</span>
