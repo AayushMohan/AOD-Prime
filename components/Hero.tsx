1d061489687f72f8eebf8a1583c76d36046dd06f
AayushMohan <aayushmohan1702@gmail.com>
2023-06-29 18:10:10 +0530

    Completed Hero Section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,7 +1,24 @@
+import Image from "next/image";
 import React from "react";
 
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <div>
+      <div>
+        <h1 className="text-7xl text-center mx-10 mt-80 px-80 text-[#FCBF49]">
+          Drive Safe, Safe Wildlife
+        </h1>
+      </div>
+      <div className="flex justify-center items-center mt-10">
+        <button className="px-4 py-3 m-4 rounded-lg bg-[#EAE2B7] text-black">
+          Learn More
+        </button>
+        <button className="px-4 py-3 m-4 rounded-lg bg-[#FCBF49] text-black">
+          Get Started
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
