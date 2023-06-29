569273904b3925e300b91ec81f548ae21ca71ae0
AayushMohan <aayushmohan1702@gmail.com>
2023-06-29 23:31:13 +0530

    Added Style in Matrics 3

--- components/Metrics.tsx
+++ components/Metrics.tsx
@@ -12,7 +12,7 @@ const Metrics = () => {
           <h1>3000</h1>
           <span>Lives Saved</span>
         </div>
-        <div className="p-40 mx-10 bg-[#0D0D0D] rounded-3xl">
+        <div className="p-40 mx-10 bg-[#0D0D0D] rounded-3xl text-center">
           <h1>77.8%</h1>
           <span>Accuracy</span>
         </div>
